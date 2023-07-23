f7c42d0bbff12d0c08f10e81fe742aa8611d110d
Tonmoysifat <tonmoysifatmd@gmail.com>
2023-07-24 01:22:56 +0600

    replaced moon-i to light-i

--- assests/js/app.js
+++ assests/js/app.js
@@ -1,5 +1,4 @@
 
-
 window.addEventListener("scroll", () => {
   if (this.scrollY > 0) {
     document.querySelector(".navbar").classList.remove("py-4");
@@ -45,19 +44,25 @@ var swiper = new Swiper(".mySwiper", {
     },
   },
 });
+
 const DarkLight = document.getElementById("DarkLight");
 const localData = localStorage.getItem("Theme");
 const homeImg = document.querySelector(".homeImg");
+const ab = document.querySelector(".ab");
 if (localStorage.getItem("Theme")==null) {
   localStorage.setItem("Theme", "light");
 };
 
 if (localData=="light") {
-  DarkLight.classList = "ri-moon-clear-fill";
+  // DarkLight.classList = "ri-moon-clear-fill";
+  DarkLight.style.color="#cdbc32";
+  ab.title="Turn off the light";
   document.body.classList.remove("darkTheme");
   homeImg.src = "./assests/img/home 2.png";
 } else if (localData == "dark") {
-  DarkLight.classList = "ri-sun-fill";
+  // DarkLight.classList = "ri-sun-fill";
+  DarkLight.style.color="#485785";
+  ab.title="Turn on the light";
   document.body.classList.add("darkTheme");
   homeImg.src = "./assests/img/home 3.png";
 }
@@ -65,11 +70,15 @@ if (localData=="light") {
 DarkLight.addEventListener("click", () => {
   document.body.classList.toggle("darkTheme");
   if (document.body.classList.contains("darkTheme")) {
-    DarkLight.classList = "ri-sun-fill";
+    // DarkLight.classList = "ri-sun-fill";
+    DarkLight.style.color="#485785";
+    ab.title="Turn on the light";
     homeImg.src = "./assests/img/home 3.png";
     localStorage.setItem("Theme", "dark");
   } else {
-    DarkLight.classList = "ri-moon-clear-fill";
+    // DarkLight.classList = "ri-moon-clear-fill";
+    DarkLight.style.color="#cdbc32";
+    ab.title="Turn off the light";
     homeImg.src = "./assests/img/home 2.png";
     localStorage.setItem("Theme", "light");
   }
